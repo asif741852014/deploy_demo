53cfefd301645b63106ae5bf651579911920643c
asif741852014 <asiftkmohammed@gmail.com>
2024-11-01 20:08:05 +0530

    push

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! The app is running on Heroku. allah you are gthe greatest');
+  res.send('Hello, World! The app is running on Heroku. allah you are gthe greatest lucky');
 });
 
 app.listen(port, () => {
